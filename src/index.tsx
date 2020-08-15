3ecff8ff39aa2f49d8cbf2c7cc513e9cfab96f03
dadada <nuhhunnuhhun@gmail.com>
2020-08-16 00:57:57 +0900

    Add demo
    with https://qiita.com/IzumiSy/items/b7d8a96eacd2cd8ad510

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import Store from "./store";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "./index.css";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={Store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root") as HTMLElement
 );
+
+// registerServiceWorker();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
