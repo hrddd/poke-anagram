c252f86a8c341c8a5d0a4a7c84319c7489fc1cb4
dadada <nuhhunnuhhun@gmail.com>
2020-09-12 23:15:26 +0900

    Rebuild anagramPuzzle module
    - initialState
    - createQuestion

--- src/redux/modules/__tests__/anagramPuzzle.test.ts
+++ src/redux/modules/__tests__/anagramPuzzle.test.ts
@@ -16,9 +16,8 @@ describe('anagramPuzzle reducer', () => {
   it('初期State', () => {
     const result = reducer(undefined, { type: 'hoge' });
     expect(result).toEqual({
-      selectedChars: [],
-      answerData: {},
-      questionData: {},
+      answers: [],
+      questions: [],
       isComplete: false,
       currentIndex: 0,
     })
@@ -29,88 +28,78 @@ describe('anagramPuzzle reducer', () => {
     // reducerに渡ったら
     const result = reducer(undefined, action);
     // 答えと問題がセットされる
-    expect(result.answerData).toEqual({
-      '1': {
-        id: '1',
-        name: 'フシギダネ',
-        order: 0
-      },
-      '20': {
-        id: '20',
-        name: 'クチート',
-        order: 1
-      },
-      '300': {
-        id: '300',
-        name: 'ポリゴン',
-        order: 2
-      }
-    });
-    const hasAllId = Object.keys(result.answerData).every((answerId) => {
-      return Object.keys(result.questionData).indexOf(answerId) >= 0
-    })
-    expect(hasAllId).toEqual(true);
-  })
-  it('selectChar: 文字を選択, deselectChar: 文字選択を解除', () => {
-    // 問題を作成し
-    const action = createQuestion(dummyData);
-    const result = reducer(undefined, action);
-    // 文字を選択すると
-    const targetCharId = Object.keys(result.questionData['1'].chars)[0];
-    const payload = {
-      questionId: '1',
-      charId: Object.keys(result.questionData['1'].chars)[0]
-    };
-    const selectCharAction = selectChar(payload);
-    const selectCharResult = reducer(result, selectCharAction);
-    // 選択状態になり
-    expect(selectCharResult.selectedChars)
-      .toContain(targetCharId)
-    // 文字の選択解除をすると
-    const deselectCharAction = deselectChar(payload);
-    const deselectCharResult = reducer(selectCharResult, deselectCharAction);
-    // 選択が解除される
-    expect(deselectCharResult.selectedChars)
-      .toEqual([])
-  })
-  it('selectChar: 文字を入れ替える', () => {
-    // 問題を作成し
-    const action = createQuestion(dummyData);
-    const result = reducer(undefined, action);
-    // 2文字選択すると
-    const questionId = '1'
-    const question = result.questionData[questionId].chars;
-    const charIds = [Object.keys(question)[0], Object.keys(question)[1]]
-    const payload = [{
-      questionId,
-      charId: charIds[0]
+    expect(result.answers).toEqual([{
+      id: '1',
+      name: 'フシギダネ'
     }, {
-      questionId,
-      charId: charIds[1]
-    }];
-    const defaultChars = result.questionData[questionId].chars;
+      id: '20',
+      name: 'クチート'
+    }, {
+      id: '300',
+      name: 'ポリゴン'
+    }]);
+    expect(result.questions).toHaveLength(3);
+  })
+  // it('selectChar: 文字を選択, deselectChar: 文字選択を解除', () => {
+  //   // 問題を作成し
+  //   const action = createQuestion(dummyData);
+  //   const result = reducer(undefined, action);
+  //   // 文字を選択すると
+  //   const targetCharId = Object.keys(result.questionData['1'].chars)[0];
+  //   const payload = {
+  //     questionId: '1',
+  //     charId: Object.keys(result.questionData['1'].chars)[0]
+  //   };
+  //   const selectCharAction = selectChar(payload);
+  //   const selectCharResult = reducer(result, selectCharAction);
+  //   // 選択状態になり
+  //   expect(selectCharResult.selectedChars)
+  //     .toContain(targetCharId)
+  //   // 文字の選択解除をすると
+  //   const deselectCharAction = deselectChar(payload);
+  //   const deselectCharResult = reducer(selectCharResult, deselectCharAction);
+  //   // 選択が解除される
+  //   expect(deselectCharResult.selectedChars)
+  //     .toEqual([])
+  // })
+  // it('selectChar: 文字を入れ替える', () => {
+  //   // 問題を作成し
+  //   const action = createQuestion(dummyData);
+  //   const result = reducer(undefined, action);
+  //   // 2文字選択すると
+  //   const questionId = '1'
+  //   const question = result.questionData[questionId].chars;
+  //   const charIds = [Object.keys(question)[0], Object.keys(question)[1]]
+  //   const payload = [{
+  //     questionId,
+  //     charId: charIds[0]
+  //   }, {
+  //     questionId,
+  //     charId: charIds[1]
+  //   }];
+  //   const defaultChars = result.questionData[questionId].chars;
 
-    const selectCharResult = reducer(result, selectChar(payload[0]));
-    const nextSelectCharResult = reducer(selectCharResult, selectChar(payload[1]));
-    const changedChars = nextSelectCharResult.questionData[questionId].chars;
-    // 文字の順番が入れ替わる
-    expect(changedChars[charIds[0]].order).toBe(defaultChars[charIds[1]].order);
-    expect(changedChars[charIds[1]].order).toBe(defaultChars[charIds[0]].order);
-  })
+  //   const selectCharResult = reducer(result, selectChar(payload[0]));
+  //   const nextSelectCharResult = reducer(selectCharResult, selectChar(payload[1]));
+  //   const changedChars = nextSelectCharResult.questionData[questionId].chars;
+  //   // 文字の順番が入れ替わる
+  //   expect(changedChars[charIds[0]].order).toBe(defaultChars[charIds[1]].order);
+  //   expect(changedChars[charIds[1]].order).toBe(defaultChars[charIds[0]].order);
+  // })
 })
-describe('anagramPuzzle selector', () => {
-  it('StateからAnagramPazzle画面用のPropsを取得', () => {
-    // createStoreし
-    const store = configureStore();
-    // selectAnagramPuzzleにstateが渡れば
-    const result = selectAnagramPuzzle(store.getState());
-    // データを返す
-    expect(result).toEqual({
-      questionData: [],
-      isComplete: false,
-      currentIndex: 0,
-      currentStep: 1,
-      maxStep: 0,
-    });
-  })
-})
+// describe('anagramPuzzle selector', () => {
+//   it('StateからAnagramPazzle画面用のPropsを取得', () => {
+//     // createStoreし
+//     const store = configureStore();
+//     // selectAnagramPuzzleにstateが渡れば
+//     const result = selectAnagramPuzzle(store.getState());
+//     // データを返す
+//     expect(result).toEqual({
+//       questionData: [],
+//       isComplete: false,
+//       currentIndex: 0,
+//       currentStep: 1,
+//       maxStep: 0,
+//     });
+//   })
+// })
--- src/redux/modules/anagramPuzzle.ts
+++ src/redux/modules/anagramPuzzle.ts
@@ -11,6 +11,9 @@ const shuffle = <T>([...array]: T[]): T[] => {
   }
   return array;
 }
+interface ICanBeNormalized {
+  id: string;
+}
 const normalizeById = <T extends ICanBeNormalized>([...array]: T[]): { [key: string]: T } => {
   if (array.length === 0 || !array[0].id) {
     throw new Error('Cant normalizeById')
@@ -22,186 +25,132 @@ const normalizeById = <T extends ICanBeNormalized>([...array]: T[]): { [key: str
     }
   }, {})
 }
-const denormalizeByOrder = <T extends ICanBeDenormalized>({ ...obj }: {
-  [key: string]: T
-}): T[] => {
-  return Object.values(obj).sort((item) => item.order)
-}
 const uniq = <T>([...array]: T[]): T[] => {
   return array.filter((elem, index, self) => self.indexOf(elem) === index);
 }
 
-// actions
-const actionCreator = actionCreatorFactory('anagramPuzzle');
-type SelectPayload = {
-  questionId: string,
-  charId: string,
-}
-export const createQuestion = actionCreator<Name[]>("CREATE_QUESTION");
-export const selectChar = actionCreator<SelectPayload>("SELECT_CHAR");
-export const deselectChar = actionCreator<SelectPayload>("DESELECT_CHAR");
-
 // state
-interface ICanBeNormalized {
-  id: string;
-}
-
-interface ICanBeDenormalized {
-  order: number;
-}
-
-type Name = {
+type Base = {
   id: string,
   name: string,
 }
 
-type QuestionBaseData = {
+type Question = {
   id: string,
-  name: string,
-  order: number
-}
-
-type AnswerData = {
-  id: string,
-  name: string,
-  order: number
-}
-
-type Char = {
-  id: string,
-  char: string,
-  order: number
-}
-
-type QuestionData = {
-  id: string,
-  order: number;
-  // 正規化された文字の分割データ
-  chars: {
-    [key: string]: Char
-  },
+  chars: string[],
 }
 
 const initialState = {
-  answerData: {} as {
-    [key: string]: AnswerData
-  },
-  questionData: {} as {
-    [key: string]: QuestionData
-  },
-  selectedChars: [] as QuestionData['id'][],
+  answers: [] as Base[],
+  questions: [] as Question[],
   isComplete: false,
   currentIndex: 0,
 };
 
+// actions
+const actionCreator = actionCreatorFactory('anagramPuzzle');
+type SelectPayload = {
+  charId: string,
+}
+export const createQuestion = actionCreator<Base[]>("CREATE_QUESTION");
+export const selectChar = actionCreator<SelectPayload>("SELECT_CHAR");
+export const deselectChar = actionCreator<SelectPayload>("DESELECT_CHAR");
+
 // reducer
-const generateQuestionData = (nameWithOrder: QuestionBaseData[]) => {
-  return normalizeById(nameWithOrder.map(({ id, name }, qIdx) => {
-    const shuffledNames = shuffle<string>(name.split(''));
+const generateQuestionData = (base: Base[]) => {
+  return base.map(({ id, name }) => {
     return {
       id,
-      order: qIdx,
-      chars: normalizeById(shuffledNames.map((char, idx) => {
-        const charId = `${id}_${char}_${idx}`;
-        return {
-          id: charId,
-          char: char,
-          order: idx
-        }
-      }))
+      chars: shuffle<string>(name.split('')),
     }
-  }))
+  })
 }
-const updateSelectedChars = (state: State, selectedChars: Char['id'][]) => {
-  return {
-    ...state,
-    selectedChars
-  }
-}
-const switchChars = (targeChars: Char['id'][], chars: QuestionData['chars']) => {
-  const firstChar = chars[targeChars[0]]
-  const secondChar = chars[targeChars[1]]
-  return {
-    ...chars,
-    [firstChar.id]: {
-      ...firstChar,
-      order: secondChar.order
-    },
-    [secondChar.id]: {
-      ...firstChar,
-      order: firstChar.order
-    }
-  }
-}
+// const updateSelectedChars = (state: State, selectedChars: Char['id'][]) => {
+//   return {
+//     ...state,
+//     selectedChars
+//   }
+// }
+// const switchChars = (targeChars: Char['id'][], chars: QuestionData['chars']) => {
+//   const firstChar = chars[targeChars[0]]
+//   const secondChar = chars[targeChars[1]]
+//   return {
+//     ...chars,
+//     [firstChar.id]: {
+//       ...firstChar,
+//       order: secondChar.order
+//     },
+//     [secondChar.id]: {
+//       ...firstChar,
+//       order: firstChar.order
+//     }
+//   }
+// }
 export const reducer = reducerWithInitialState(initialState)
-  .case(createQuestion, (state, name) => {
-    const nameWithOrder = name.map((item, idx) => ({
-      ...item,
-      order: idx
-    }))
+  .case(createQuestion, (state, baseData) => {
     return {
       ...state,
-      answerData: normalizeById(nameWithOrder),
-      questionData: generateQuestionData(nameWithOrder)
+      answers: [...baseData],
+      questions: generateQuestionData(baseData)
     }
   })
-  .case(selectChar, (state, { questionId, charId }) => {
-    const selectedChars = uniq([...state.selectedChars, charId])
-    if (selectedChars.length === 1) {
-      // 選択状態が一個ならそのまま反映
-      return updateSelectedChars(state, selectedChars)
-    } else if (selectedChars.length === 2) {
-      // 選択状態が二個なら順番を入れ替えて選択状態を解除
-      return {
-        ...state,
-        questionData: {
-          ...state.questionData,
-          selectedChars: [],
-          [questionId]: {
-            ...state.questionData[questionId],
-            chars: switchChars(selectedChars, state.questionData[questionId].chars)
-          }
-        }
-      }
-    } else {
-      // それ以外は選択状態を解除するのみ
-      return {
-        ...state,
-        questionData: {
-          ...state.questionData,
-          [questionId]: {
-            ...state.questionData[questionId],
-            selectedChars: []
-          }
-        }
-      }
-    }
-  })
-  .case(deselectChar, (state, { questionId, charId }) => {
-    const selectedChars = state.questionData[questionId].selectedChars;
-    return {
-      ...state,
-      questionData: {
-        ...state.questionData,
-        [questionId]: {
-          ...state.questionData[questionId],
-          selectedChars: [...selectedChars].splice(selectedChars.indexOf(charId), 1)
-        }
-      }
-    }
-  })
+//   .case(selectChar, (state, { questionId, charId }) => {
+//     const selectedChars = uniq([...state.selectedChars, charId])
+//     if (selectedChars.length === 1) {
+//       // 選択状態が一個ならそのまま反映
+//       return updateSelectedChars(state, selectedChars)
+//     } else if (selectedChars.length === 2) {
+//       // 選択状態が二個なら順番を入れ替えて選択状態を解除
+//       return {
+//         ...state,
+//         questionData: {
+//           ...state.questionData,
+//           selectedChars: [],
+//           [questionId]: {
+//             ...state.questionData[questionId],
+//             chars: switchChars(selectedChars, state.questionData[questionId].chars)
+//           }
+//         }
+//       }
+//     } else {
+//       // それ以外は選択状態を解除するのみ
+//       return {
+//         ...state,
+//         questionData: {
+//           ...state.questionData,
+//           [questionId]: {
+//             ...state.questionData[questionId],
+//             selectedChars: []
+//           }
+//         }
+//       }
+//     }
+//   })
+//   .case(deselectChar, (state, { questionId, charId }) => {
+//     const selectedChars = state.questionData[questionId].selectedChars;
+//     return {
+//       ...state,
+//       questionData: {
+//         ...state.questionData,
+//         [questionId]: {
+//           ...state.questionData[questionId],
+//           selectedChars: [...selectedChars].splice(selectedChars.indexOf(charId), 1)
+//         }
+//       }
+//     }
+//   })
 
 // selector
-export const selectAnagramPuzzle = createSelector(
-  (state: RootState) => state.ui.anagramPuzzle,
-  (anagramPuzzle) => ({
-    questionData: denormalizeByOrder(anagramPuzzle.questionData),
-    isComplete: anagramPuzzle.isComplete,
-    currentIndex: anagramPuzzle.currentIndex,
-    currentStep: anagramPuzzle.currentIndex + 1,
-    maxStep: Object.keys(anagramPuzzle.answerData).length,
-  })
-);
+// export const selectAnagramPuzzle = createSelector(
+//   (state: RootState) => state.ui.anagramPuzzle,
+//   ({ questions, isComplete, currentIndex }) => ({
+//     questions,
+//     isComplete,
+//     currentIndex,
+//     currentStep: currentIndex + 1,
+//     maxStep: Object.keys(questions).length,
+//   })
+// );
 
-export type SelectedAnagramPuzzle = ReturnType<typeof selectAnagramPuzzle>;
+// export type SelectedAnagramPuzzle = ReturnType<typeof selectAnagramPuzzle>;
 export type State = typeof initialState;
